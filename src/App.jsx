478d72d663d828c508b14b03f512c405fb77565d
alex-zander-0403 <alexzander0403@gmail.com>
2025-12-27 13:38:27 +0300

    fix: add main component (Todo.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
-import AddTaskForm from "./components/AddTaskForm";
-import SearchTaskForm from "./components/SearchTaskForm";
-import TodoInfo from "./components/TodoInfo";
-import TodoList from "./components/TodoList";
+import Todo from "./components/Todo";
 
 function App() {
-  return (
-    <div className="todo">
-      <h1 className="todo__title">To Do List</h1>
-
-      <AddTaskForm />
-      <SearchTaskForm />
-
-      <TodoInfo />
-      <TodoList />
-    </div>
-  );
+  return <Todo />;
 }
 
 export default App;
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,3 +1,5 @@
+import TodoItem from "./TodoItem";
+
 function TodoList() {
   const hasTasks = true;
 
