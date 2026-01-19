696197b98a8980e052cb33a6bb38cd405ba88c5f
alex-zander-0403 <alexzander0403@gmail.com>
2026-01-19 12:38:58 +0300

    fead(css): add deleting animation

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,8 +1,8 @@
-import { memo, useContext } from "react";
-
+import { memo, useContext, useRef } from "react";
 import { TasksContext } from "../../context/TasksContext";
 import RouterLink from "../RouterLink/RouterLink";
 import styles from "./TodoItem.module.scss";
+import useCombinedRefs from "../../hooks/useCombineRefs";
 
 function TodoItem(props) {
   const { className = "", id, title, isDone } = props;
@@ -15,11 +15,21 @@ function TodoItem(props) {
     toggleTaskComplete,
   } = useContext(TasksContext);
 
+  const animationRef = useRef(null);
+  const combinedRef = useCombinedRefs(
+    id === firstIncompleteTaskId ? firstIncompleteTaskRef : null,
+    animationRef,
+  );
+
+  const handleClick = () => {
+    animationRef.current?.classList.add(styles.isDisappearing);
+    setTimeout(() => {
+      deleteTask(id);
+    }, 400);
+  };
+
   return (
-    <li
-      className={`${styles.todoItem} ${className}`}
-      ref={id === firstIncompleteTaskId ? firstIncompleteTaskRef : null}
-    >
+    <li className={`${styles.todoItem} ${className} `} ref={combinedRef}>
       <input
         className={styles.todoItem__checkbox}
         id={id}
@@ -45,7 +55,7 @@ function TodoItem(props) {
         className={styles.todoItem__deleteButton}
         aria-label="Delete"
         title="Delete"
-        onClick={() => deleteTask(id)}
+        onClick={handleClick}
       >
         <svg
           width="20"
